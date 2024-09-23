7c27510ee26d14de7c7bd6cd476bcd86f405c12c
RAHUL JAIN <rahulrd.jain@gmail.com>
2024-09-23 21:54:30 +0530

    Initial commit

---
+++ src/main/java/com/qa/demo/RestAsurd/App.java
@@ -0,0 +1,10 @@
+package com.qa.demo.RestAsurd;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/com/qa/demo/RestAsurd/AppTest.java
@@ -0,0 +1,33 @@
+package com.qa.demo.RestAsurd;
+
+
+
+import org.testng.annotations.Test;
+import org.testng.asserts.Assertion;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+
+
+public class AppTest {
+
+ 
+    
+	@Test
+    public void testGetMethod() {
+        
+    	RestAssured.baseURI = "https://bookstore.toolsqa.com/";  //URI
+    	RequestSpecification req =  RestAssured.given();
+    	Response res = req.get("BookStore/v1/Books");
+    	
+    	System.out.println("Status:"+res.getStatusCode());
+    	System.out.println("Response:"+ res.prettyPrint());
+    	
+    	Assertion assertion = new Assertion();
+		assertion.assertEquals(200, res.getStatusCode());
+    	
+    	
+    }
+}
