e2e43fd02f92426fc5663de2be54233c2e99e8b2
RAHUL JAIN <rahulrd.jain@gmail.com>
2024-09-28 23:53:45 +0530

    Testing

--- src/main/java/com/qa/demo/RestAsurd/App.java
+++ src/main/java/com/qa/demo/RestAsurd/App.java
@@ -5,6 +5,7 @@ package com.qa.demo.RestAsurd;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello!")
+        ;
     }
 }
